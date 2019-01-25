9814b4ad78043223cdd88dcbcb7cde1f58c45a2b
Philip Lindsay <44672323+redthre3@users.noreply.github.com>
2019-01-25 17:40:50 -0500

    update

---
+++ OhmLawCalc/Battery.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OhmLawCalc
+{
+    class Battery
+    {
+        #region FIELDS
+        private string _brand;
+        private double _mah;
+        private double _amp;
+        private string _size;
+        private double _volt;
+
+        #endregion
+
+        #region PROPERTIES
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+        public double MAH
+        {
+            get { return _mah; }
+            set { _mah = value; }
+        }
+
+        public double Amp
+        {
+            get { return _amp; }
+            set { _amp = value; }
+        }
+        public string Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+        public double Volt
+        {
+            get { return _volt; }
+            set { _volt = value; }
+        }
+        #endregion
+
+        #region CONSTRUCTORS
+        public Battery()
+        {
+
+        }
+        #endregion
+
+        #region METHODS
+
+        #endregion
+
+    }
+}
+
--- OhmLawCalc/MainWindow.xaml.cs
+++ OhmLawCalc/MainWindow.xaml.cs
@@ -22,7 +22,72 @@ namespace OhmLawCalc
     {
         public MainWindow()
         {
+            bool[] equation = new bool[3];
+            
             InitializeComponent();
+        }
+
+        private void Radio_Button_Voltage_Checked(object sender, RoutedEventArgs e)
+        {
+            
+            TextBox_Voltage.IsReadOnly = true;
+            TextBox_Resistance.IsReadOnly = false;
+            TextBox_Current.IsReadOnly = false;
+            TextBox_Voltage.Background = Brushes.Gray;
+            TextBox_Resistance.Background = Brushes.WhiteSmoke;
+            TextBox_Current.Background = Brushes.WhiteSmoke;
+           
+        }
+
+        private void Radio_Button_Resistance_Checked(object sender, RoutedEventArgs e)
+        {
+           
+            TextBox_Voltage.IsReadOnly = false;
+            TextBox_Resistance.IsReadOnly = true;
+            TextBox_Current.IsReadOnly = false;
+            TextBox_Voltage.Background = Brushes.WhiteSmoke;
+            TextBox_Resistance.Background = Brushes.Gray;
+            TextBox_Current.Background = Brushes.WhiteSmoke;
+            
+        }
+
+        private void Radio_Button_Current_Checked(object sender, RoutedEventArgs e)
+        {
+            
+            TextBox_Voltage.IsReadOnly = false;
+            TextBox_Resistance.IsReadOnly = false;
+            TextBox_Current.IsReadOnly = true;
+            TextBox_Voltage.Background = Brushes.WhiteSmoke;
+            TextBox_Resistance.Background = Brushes.WhiteSmoke;
+            TextBox_Current.Background = Brushes.Gray;
+           
+            
+        }
+
+        private void GetEquation(bool[] equation)
+        {
+            double R, A, V;
+            if (Radio_Button_Voltage.IsChecked.Value)
+            {
+                double.TryParse(TextBox_Current.Text, out A);
+                double.TryParse(TextBox_Resistance.Text, out R);
+                V = A * R;
+                TextBox_Voltage.Text = V.ToString();
+            }
+            else if (Radio_Button_Resistance.IsChecked.Value)
+            {
+                double.TryParse(TextBox_Voltage.Text, out V);
+                double.TryParse(TextBox_Current.Text, out A);
+                R = V / A;
+                TextBox_Resistance.Text = R.ToString();
+            }
+            else if (Radio_Button_Current.IsChecked.Value)
+            {
+                double.TryParse(TextBox_Voltage.Text, out V);
+                double.TryParse(TextBox_Resistance.Text, out R);
+                A= V /R;
+                TextBox_Resistance.Text = A.ToString();
+            }
         }
     }
 }
